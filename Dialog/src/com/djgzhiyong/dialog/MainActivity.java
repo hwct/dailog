2fe7dcc846019b8f60b497df2ef6d54ea9e7b02b
hwctchen@foxmail.com <Administrator@PC-20151009CCEJ>
2015-10-16 10:32:57 +0800

    我真改了

--- Dialog/src/com/djgzhiyong/dialog/MainActivity.java
+++ Dialog/src/com/djgzhiyong/dialog/MainActivity.java
@@ -30,21 +30,21 @@ public class MainActivity extends Activity implements OnClickListener {
 		Dialog dialog = new Dialog(this, R.style.transparentFrameWindowStyle);
 		dialog.setContentView(view, new LayoutParams(LayoutParams.FILL_PARENT,
 				LayoutParams.WRAP_CONTENT));
-		Window window = dialog.getWindow();
-		// 设置显示动画
-		window.setWindowAnimations(R.style.main_menu_animstyle);
-		WindowManager.LayoutParams wl = window.getAttributes();
-		wl.x = 0;
-		wl.y = getWindowManager().getDefaultDisplay().getHeight();
-		// 以下这两句是为了保证按钮可以水平满屏
-		wl.width = ViewGroup.LayoutParams.MATCH_PARENT;
-		wl.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-
-		
-		// 设置显示位置
-		dialog.onWindowAttributesChanged(wl);
-		// 设置点击外围解散
-		dialog.setCanceledOnTouchOutside(true);
-		dialog.show();
+//		Window window = dialog.getWindow();
+//		// 设置显示动画
+//		window.setWindowAnimations(R.style.main_menu_animstyle);
+//		WindowManager.LayoutParams wl = window.getAttributes();
+//		wl.x = 0;
+//		wl.y = getWindowManager().getDefaultDisplay().getHeight();
+//		// 以下这两句是为了保证按钮可以水平满屏
+//		wl.width = ViewGroup.LayoutParams.MATCH_PARENT;
+//		wl.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+//
+//		
+//		// 设置显示位置
+//		dialog.onWindowAttributesChanged(wl);
+//		// 设置点击外围解散
+//		dialog.setCanceledOnTouchOutside(true);
+//		dialog.show();
 	}
 }
